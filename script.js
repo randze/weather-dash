d953f65e3b3fafd058eb7b414923864a5552c29c
randze <65006180+randze@users.noreply.github.com>
2020-06-12 02:12:57 -0400

    Added weather icons and localstorage save

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 const today = new Date();
 let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
 const APIkey = '24efea332a9cf0abcfd7b79b7c7be057'
 var weatherInfo
 var indexUV
 var fiveDay
 
+// localstorage variables
+var haveLastSearch = localStorage.searchSave ? true : false
 var searchSave = localStorage.searchSave ? JSON.parse(localStorage.searchSave) : []
+
+if (haveLastSearch) {
+    genHistoryList()
+
+    let weatherOne = JSON.parse(localStorage.saveOne)
+    let weatherTwo = JSON.parse(localStorage.saveTwo)
+
+    document.querySelector('#generalWeather').innerHTML = weatherOne
+    document.querySelector('#fiveDay').innerHTML = weatherTwo
+
+    let indexNum = document.querySelector('#uvExposure').textContent
+    uvCheck(indexNum)
+}
+
+function genHistoryList() {
+    for (var idx = 0 ; idx < searchSave.length ; idx++) {
+        document.querySelector('#searchHistory').innerHTML +=
+        `
+        <tr>
+            <td scope="row">${searchSave[idx]}</td>
+        </tr>
+        `
+    }
+}
 
 async function citySearch() {
     event.preventDefault()
 
     let cityName = document.querySelector('#cityBox').value
-    let apiLink = 'http://api.openweathermap.org/data/2.5/weather?q='+cityName+'&units=metric&appid='+APIkey
+    let apiLink = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${APIkey}`
     
-    document.querySelector('#searchHistory').innerHTML +=
-    `
-    <tr>
-        <td scope="row">${cityName}</td>
-    </tr>
-    `
-
     await fetch(apiLink)
     .then(response => response.json())
     .then(function(response){
@@ -44,19 +64,20 @@ async function citySearch() {
     displayWeather()
 }
 
+// Function to generate weather information found
 function displayWeather() {
-    document.querySelector('#generalWeather').innerHTML =
+    var weatherOne = document.querySelector('#generalWeather').innerHTML =
     `
-    <h2>${weatherInfo.name} [ ${date} ]<img class="" src="http://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png"></h2>
+    <h2>${weatherInfo.name} [ ${date} ]<img class="" src="http://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}.png"></h2>
     <p>Temperature: ${weatherInfo.main.temp}&deg;C</p>
     <p>Humidity: ${weatherInfo.main.humidity}%</p>
     <p>Wind Speed: ${weatherInfo.wind.speed} km/h</p>
     <p>UV Index: <span id="uvExposure">${indexUV[0].value}</span></p>
     `
-    uvCheck(indexUV[0].value)
 
-    searchSave.push(weatherInfo.name)
-    localStorage.searchSave = JSON.stringify(searchSave)
+    searchSave.push(`${weatherInfo.name}, ${weatherInfo.sys.country}`)
+
+    uvCheck(indexUV[0].value)
     
     // Clear old 5day search
     document.querySelector('#fiveDay').innerHTML = ``
@@ -67,17 +88,31 @@ function displayWeather() {
         var celTemp = Math.round(Number(fiveDay.list[i].main.temp))-273.15
         var celTempCalc = celTemp.toFixed(2)
 
-        document.querySelector('#fiveDay').innerHTML +=
+        var weatherTwo = document.querySelector('#fiveDay').innerHTML +=
         `
-        <div class="col-12 col-sm-6 col-md-4 col-lg myCard">
+        <div class="col-12 col-sm-6 col-md-6 col-lg myCard">
             <p style="font-weight:bold;">${shortDate}</p>
             <img class="" src="http://openweathermap.org/img/wn/${fiveDay.list[i].weather[0].icon}.png">
             <p>Temp: ${celTempCalc}&deg;C</p>
-            <p>Humidity:${fiveDay.list[i].main.humidity}%</p>
+            <p>Humidity: ${fiveDay.list[i].main.humidity}%</p>
         </div>
         `
     }
+
+    localSave(weatherOne, weatherTwo)
+    
+    // Display search history
+    genHistoryList()
+    
 }
+
+function localSave(saveOne, saveTwo) {
+    haveLastSearch = true
+    localStorage.searchSave = JSON.stringify(searchSave)
+    localStorage.saveOne = JSON.stringify(saveOne)
+    localStorage.saveTwo = JSON.stringify(saveTwo)
+}
+
 
 // favorable, moderate, or severe UVindex based on "https://www.canada.ca/en/environment-climate-change/services/weather-health/uv-index-sun-safety.html"
 function uvCheck(value) {
@@ -89,3 +124,6 @@ function uvCheck(value) {
         document.getElementById("uvExposure").style.background="green";
     }
 }
+
+
+
